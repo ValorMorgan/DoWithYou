be057d2cb16d293b089f0a28a2ca71f5c4edbce6
ValorMorgan <scriptcoded@hotmail.com>
2018-01-29 12:57:31 -0500

    CSS transformed to SCSS (SASS).  Updated Gulp to work with SASS.

--- DoWithYou/gulpfile.js
+++ DoWithYou/gulpfile.js
@@ -1,87 +1,142 @@
-﻿/// <binding BeforeBuild='clean' AfterBuild='min' Clean='clean, min' />
+﻿/// <binding BeforeBuild='clean' AfterBuild='copy-assets, min' Clean='clean' />
 'use strict';
 
+// Includes
 var gulp = require('gulp'),
     rimraf = require('rimraf'),
     concat = require('gulp-concat'),
     cssmin = require('gulp-cssmin'),
     uglify = require('gulp-uglify'),
+    sass = require('gulp-sass'),
     _ = require('lodash');
 
-var webroot = './wwwroot/',
-    libRoot = webroot + 'lib/',
-    jsRoot = webroot + 'js/',
-    cssRoot = webroot + 'css/',
-    jsExt = '*.js',
-    jsMinExt = '*.min.js',
-    cssExt = '*.css',
-    cssMinExt = '*.min.css',
-    mapExt = '*.map';
+var roots = {
+    web: './wwwroot/',
+    node: './node_modules/'
+};
+
+var branches = {
+    lib: roots.web + 'lib/',
+    js: roots.web + 'js/',
+    css: roots.web + 'css/',
+    scss: roots.web + 'css/' + 'scss/'
+};
+
+var extensions = {
+    js: '*.js',
+    jsMin: '*.min.js',
+    css: '*.css',
+    cssMin: '*.min.css',
+    scss: '*.scss',
+    map: '*.map'
+};
 
 var paths = {
-    js: jsRoot + '**/' + jsExt,
-    minJs: jsRoot + '**/' + jsMinExt,
-    css: cssRoot + '**/' + cssExt,
-    minCss: cssRoot + '**/' + cssMinExt,
-    concatJsDest: jsRoot + 'site.min.js',
-    concatCssDest: cssRoot + 'site.min.css'
+    js: branches.js + '**/' + extensions.js,
+    minJs: branches.js + '**/' + extensions.jsMin,
+    css: branches.css + '**/' + extensions.css,
+    scss: branches.scss + '**/' + extensions.scss,
+    minCss: branches.css + '**/' + extensions.cssMin,
+    sassCssDest: roots.web + 'css',
+    concatJsDest: branches.js + 'site.min.js',
+    concatCssDest: branches.css + 'site.min.css'
 };
+
+gulp.task('clean:lib:dist',
+    function(cb) {
+        var path = branches.lib + '**/dist/**/*.*';
+        console.log('rimraf: ' + path);
+        rimraf(path, cb);
+    });
+
+gulp.task('clean:lib:bundles',
+    function(cb) {
+        var path = branches.lib + '**/bundles/**/*.*';
+        console.log('rimraf: ' + path);
+        rimraf(path, cb);
+    });
+
+gulp.task('clean:lib:lib',
+    function(cb) {
+        var path = branches.lib + '**/lib/**/*.*';
+        console.log('rimraf: ' + path);
+        rimraf(path, cb);
+    });
+
+gulp.task('clean:lib',
+    ['clean:lib:dist', 'clean:lib:bundles', 'clean:lib:lib'],
+    function(cb) {
+        console.log('rimraf: ' + branches.lib);
+        rimraf(branches.lib, cb);
+    });
 
 gulp.task('copy-assets',
     function () {
-        var assetRoot = './node_modules/';
-
-        // All assets
         var assets = {
             js: [
-                assetRoot + '**/dist/' + jsExt,
-                assetRoot + '**/dist/js/' + jsExt,
-                assetRoot + '**/bundles/' + jsExt,
-                assetRoot + '**/lib/' + jsExt
+                roots.node + '**/' + extensions.js,
+                roots.node + '**/' + extensions.jsMin
             ],
             css: [
-                assetRoot + '**/dist/' + cssExt,
-                assetRoot + '**/dist/css/' + cssExt,
-                assetRoot + '**/bundles/' + cssExt,
-                assetRoot + '**/lib/' + cssExt
+                roots.node + '**/' + extensions.css,
+                roots.node + '**/' + extensions.cssMin
             ],
             map: [
-                assetRoot + '**/dist/' + mapExt,
-                assetRoot + '**/dist/js/' + mapExt,
-                assetRoot + '**/dist/css/' + mapExt,
-                assetRoot + '**/bundles/' + mapExt,
-                assetRoot + '**/lib/' + mapExt
+                roots.node + '**/' + extensions.map
             ]
         };
 
+        var excludes = [
+            '!' + roots.node + '**/' + 'node_modules/**/*.*',
+            '!' + roots.node + '**/' + 'src/**/*.*',
+            '!' + roots.node + '**/' + 'example/**/*.*',
+            '!' + roots.node + '**/' + 'examples/**/*.*',
+            '!' + roots.node + '**/' + 'test/**/*.*',
+            '!' + roots.node + '**/' + 'locale/**/*.*'
+        ];
+
         _(assets).forEach(function (asset, type) {
-            gulp.src(asset)
-                .pipe(gulp.dest(libRoot));
+            var final = asset.concat(excludes);
+            console.log('Pipe: ' + final);
+            gulp.src(final)
+                .pipe(gulp.dest(branches.lib));
         });
     });
 
 gulp.task('clean:js',
-    function(cb) {
+    function (cb) {
+        console.log('rimraf: ' + paths.concatJsDest);
         rimraf(paths.concatJsDest, cb);
     });
 
 gulp.task('clean:css',
-    function(cb) {
+    function (cb) {
+        console.log('rimraf: ' + paths.concatCssDest);
         rimraf(paths.concatCssDest, cb);
     });
 
-gulp.task('clean', ['clean:js', 'clean:css']);
+gulp.task('clean', ['clean:js', 'clean:css', 'clean:lib']);
+
+gulp.task('sass',
+    function () {
+        console.log('Sass: ' + paths.scss + ' -> ' + paths.sassCssDest);
+        return gulp.src(paths.scss)
+            .pipe(sass().on('error', sass.logError))
+            .pipe(gulp.dest(paths.sassCssDest));
+    });
 
 gulp.task('min:js',
-    function() {
+    function () {
+        console.log('Minify: ' + paths.js);
         return gulp.src([paths.js, '!' + paths.minJs], { base: '.' })
             .pipe(concat(paths.concatJsDest))
             .pipe(uglify())
             .pipe(gulp.dest('.'));
     });
 
-gulp.task('min:css',
-    function() {
+gulp.task('min:css', ['sass'],
+    function () {
+        console.log('Minify: ' + paths.css);
         return gulp.src([paths.css, '!' + paths.minCss])
             .pipe(concat(paths.concatCssDest))
             .pipe(cssmin())
