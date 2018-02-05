beb32de6469674dffc56339c7875c80011e7d599
Joshua Morgan <scriptcoded@hotmail.com>
2018-02-04 20:28:19 -0500

    Working on buttons (may use mincss).

---
+++ DoWithYou.UI.React/ClientApp/components/Button.tsx
@@ -0,0 +1,13 @@
+﻿import * as React from 'react';
+
+export class Button extends React.Component<{}, {}> {
+    constructor() {
+        super();
+    }
+
+    public render() {
+        return (
+            <button className='button'>{this.props.children}</button>
+        );
+    }
+}
---
+++ DoWithYou.UI.React/ClientApp/components/Header.tsx
@@ -0,0 +1,11 @@
+﻿import * as React from 'react';
+import { Appbar } from 'muicss/react';
+
+export class Header extends React.Component<{}, {}> {
+    public render() {
+        return (
+            <Appbar>
+            </Appbar>
+        );
+    }
+}
