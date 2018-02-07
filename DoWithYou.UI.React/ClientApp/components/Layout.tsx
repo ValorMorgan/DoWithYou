e7b5c808621536a7c72bb5bd6339ff7002797dd3
ValorMorgan <scriptcoded@hotmail.com>
2018-02-07 15:29:28 -0500

    Added media styling for all styles (defined by Bootstrap).  Side-nav now dynamically moves with screen resizing.

--- DoWithYou.UI.React/ClientApp/components/Layout.tsx
+++ DoWithYou.UI.React/ClientApp/components/Layout.tsx
@@ -12,11 +12,11 @@ export class Layout extends React.Component<ILayoutProps, {}> {
         return (
             <div className='container-fluid'>
                 <div className='row'>
-                    <div className='col-sm-3'>
+                    <div className='col-sm-2'>
                         <NavMenu />
                     </div>
                     <Misc.ClearFix />
-                    <div className='col-sm-9'>
+                    <div className='col-sm-10'>
                         <Header />
                         {this.props.children}
                     </div>
